355678ff465d02940cbc5b727ed14980b790724c
isaacribeiro <ribeiro.isaac@hotmail.com>
2018-05-30 21:59:22 -0300

    geopositioning update - porto alegre

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Brazilian Income by City
+# Brazilian Income _per capita_ by City
 ## UNISINOS - Estatística Aplicada / 2018
 
 Data extracted from [Atlas Brasil](http://www.atlasbrasil.org.br/2013/pt/download/)
--- project.R
+++ project.R
@@ -1,1 +1,23 @@
-dataset <- read_xlsx("data/DADOS_DO_ATLAS_DESAGREGADOS_POR_COR__SEXO_E_DOMICILIO_-_2000_E_2010_-_FINAL.xlsx", header=TRUE)
+library(maptools)
+porto_alegre <- readShapePoly("./data/porto_alegre/SH RM_Porto_Alegre/RM_PortoAlegre_Regional_region.shp")
+
+library(RColorBrewer)
+colors <- brewer.pal(9, "PuBuGn")
+
+x_mean = format(round(mean(porto_alegre.points[,1]),2))
+y_mean = format(round(mean(porto_alegre.points[,2]),2))
+
+library(ggmap)
+mapImage <- get_map(location = c(lon = -51.16, lat = -30.09),
+                    color = "color",
+                    source = "google",
+                    maptype = "satellite",
+                    zoom = 11)
+
+porto_alegre.points <- fortify(porto_alegre)
+
+ggmap(mapImage) + geom_polygon(aes(x = long, y = lat, group = group),
+                               data = porto_alegre.points,
+                               color = colors[9],
+                               fill = colors[6],
+                               alpha = 0.5) + labs(x = "Latitude", y = "Longitude")
