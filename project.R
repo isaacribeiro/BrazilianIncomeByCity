d5523c63ceb5f6359e3c56520ea3c248fe8b20ca
isaacribeiro <ribeiro.isaac@hotmail.com>
2018-05-30 23:25:09 -0300

    clean code

--- project.R
+++ project.R
@@ -1,12 +1,17 @@
+#Mandatory Packages
+download.packages(maptools)
+download.packages(RColorBrewer)
+download.packages(ggmap)
+
+#load shapeFile from data repository
 library(maptools)
 porto_alegre <- readShapePoly("./data/porto_alegre/SH RM_Porto_Alegre/RM_PortoAlegre_Regional_region.shp")
 
+#load color palette for map integration
 library(RColorBrewer)
 colors <- brewer.pal(9, "PuBuGn")
 
-x_mean = format(round(mean(porto_alegre.points[,1]),2))
-y_mean = format(round(mean(porto_alegre.points[,2]),2))
-
+#retrieve map
 library(ggmap)
 mapImage <- get_map(location = c(lon = -51.16, lat = -30.09),
                     color = "color",
@@ -14,10 +19,13 @@ mapImage <- get_map(location = c(lon = -51.16, lat = -30.09),
                     maptype = "satellite",
                     zoom = 11)
 
+#convert shape file into a data frame format for integration with GIS
 porto_alegre.points <- fortify(porto_alegre)
 
-ggmap(mapImage) + geom_polygon(aes(x = long, y = lat, group = group),
-                               data = porto_alegre.points,
-                               color = colors[9],
-                               fill = colors[6],
-                               alpha = 0.5) + labs(x = "Latitude", y = "Longitude")
+ggmap(mapImage) + 
+geom_polygon(aes(x = long, y = lat, group = group),
+                     data = porto_alegre.points,
+                     color = colors[9],
+                     fill = colors[6],
+                     alpha = 0.5) + 
+slabs(x = "Latitude", y = "Longitude")
